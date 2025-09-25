c88ce920a7305394f028c9845d7a9a58f0eae0ce
Yvain <yvain.ye@gmail.com>
2025-09-25 15:10:49 +0200

    feat: localhost:2317 will now show a test page

---
+++ front/geneweb_app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    lang = request.args.get("lang", "en").lower()
+    valid_langs = ["de", "en", "es", "fr", "it", "lv", "nl", "no", "fi", "sv"]
+    if lang not in valid_langs:
+        lang = "en"
+    return render_template("welcome.html", lang=lang)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
