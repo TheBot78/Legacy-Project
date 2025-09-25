22d4043ec1ee1aae57a56f41a2d0d7f01e42c287
Yvain <yvain.ye@gmail.com>
2025-09-25 13:27:48 +0200

    feat: added the START.html file

---
+++ front/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, jsonify, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("main.html")
+
+@app.route("/ping_rpc")
+def ping_rpc():
+    try:
+        # Appel RPC interne Docker
+        response = requests.post(
+            "http://rpc:2316/rpc",
+            json={"method": "ping", "params": {}}
+        )
+        rpc_result = response.json().get("result", "Pas de résultat")
+    except Exception as e:
+        rpc_result = f"Erreur RPC : {e}"
+    return jsonify({"result": rpc_result})
+
+@app.route("/geneweb")
+def geneweb():
+    return render_template("geneweb.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
