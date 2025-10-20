face33ba46ae442a0f5d6ab4a190d0167421724e
Yvain Ye <yvainye@MacBook-Pro-de-Yvain.local>
2025-10-20 14:13:14 +0200

    feat: It will check if the user have a db or not, if not it will redirect to the choose a genealogy page, otherwise it will load the main.html page

--- front/app.py
+++ front/app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, jsonify, request
 import requests
+import os
 
 app = Flask(__name__)
 
+def check_db_exists():
+    db_path = os.path.join(os.getcwd(), "db")
+    if not os.path.exists(db_path):
+        return False
+    files = [f for f in os.listdir(db_path) if os.path.isfile(os.path.join(db_path, f))]
+    return len(files) > 0
+
+
 @app.route("/")
 def home():
+    if not check_db_exists(): # If no database found, redirect to choose genealogy ottherwise main page
+        return render_template("choose_genealogy.html")
     return render_template("main.html")
+
 
 @app.route("/ping_rpc")
 def ping_rpc():
     try:
-        # Appel RPC interne Docker
         response = requests.post(
             "http://rpc:2316/rpc",
             json={"method": "ping", "params": {}}
@@ -20,15 +31,23 @@ def ping_rpc():
         rpc_result = f"Erreur RPC : {e}"
     return jsonify({"result": rpc_result})
 
+
 @app.route("/geneweb")
 def geneweb():
     lang = request.args.get('lang', 'fr')
     return render_template("geneweb.html", lang=lang)
 
+
 @app.route("/welcome")
 def welcome():
     lang = request.args.get('lang', 'fr')
     return render_template("welcome.html", lang=lang)
+
+
+@app.route("/choose_genealogy")
+def choose_genealogy():
+    return render_template("choose_genealogy.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
