45107c2edffc4bb97a63b78aebfbd3da0f882497
Yvain Ye <yvain.ye@gmail.com>
2025-10-28 13:33:27 +0100

    feat: added the Import a GEDCOM file page, with the file navigator

--- front/gwsetup/app.py
+++ front/gwsetup/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from path import list_dir
 
 app = Flask(
     __name__,
@@ -14,6 +15,19 @@ def home():
 def welcome():
     lang = request.args.get("lang", "fr")
     return render_template("welcome.html", lang=lang)
+
+@app.route("/ged2gwb")
+def ged2gwb():
+    sel = request.args.get("sel", "/")
+    lang = request.args.get("lang", "en")
+    abs_sel, items, parent = list_dir(sel)
+    return render_template(
+        "Management_Creation/ged2gwb.html",
+        sel=abs_sel,
+        items=items,
+        parent=parent,
+        lang=lang  # <-- AJOUTEZ CETTE LIGNE
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2316, debug=True)
