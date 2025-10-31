48c5fb2408128bbc276b217508b1f38009d96437
Fylga <joris.francin@epitech.eu>
2025-10-31 09:02:00 +0100

    feat: test start app

---
+++ tests/unit/test_start_app.py
@@ -0,0 +1,15 @@
+import pytest
+from front.start.start_app import app # Import the 'start' app
+
+@pytest.fixture
+def client():
+    """Pytest-Flask fixture to create a test client for front/start/start_app.py."""
+    with app.test_client() as client:
+        yield client
+
+def test_start_root_redirects_to_welcome(client):
+    """
+    Tests that the / route redirects to /welcome.
+    """
+    response = client.get('/', follow_redirects=False)
+    assert response.status_code == 200
