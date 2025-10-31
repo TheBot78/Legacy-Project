e6c82fc307376e3d8d1bd2b18490fe8411d8597f
Fylga <joris.francin@epitech.eu>
2025-10-31 09:59:05 +0100

    feat: added 404 error pathing handling test

--- tests/unit/test_start_app.py
+++ tests/unit/test_start_app.py
@@ -13,3 +13,10 @@ def test_start_root_redirects_to_welcome(client):
     """
     response = client.get('/', follow_redirects=False)
     assert response.status_code == 200
+
+def test_start_invalid_path_access(client):
+    """
+    Tests that the / route redirects to /welcome.
+    """
+    response = client.get('/a', follow_redirects=False)
+    assert response.status_code == 404
